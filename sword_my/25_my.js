265aa897ec2de18bf7f8db267f2bcca4214edd00
zihao peng <pzh702@gmail.com>
2021-08-12 17:22:40 +0800

    合并有序数组，递归

--- sword_my/25_my.js
+++ sword_my/25_my.js
@@ -5,4 +5,13 @@ function mergeList(head1, head2) {
 	if (!head2) {
 		return head1;
 	}
+	let node = null;
+	if (head1.val < head2.val) {
+		node = head1;
+		node.next = mergeList(head1.next, head2);
+	} else {
+		node = head2;
+		node.next = mergeList(head1, head2.next);
+	}
+	return node;
 }
